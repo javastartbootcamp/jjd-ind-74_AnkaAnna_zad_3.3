bdb104b4cad25a64f9d4c7ad578d4bbc516999b6
Anna M <ankaanna@gmail.com>
2023-09-16 23:42:43 +0200

    rozwiazanie3.3

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,5 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Math math = new Math();
+        int number = 5;
+        boolean even = math.isEven(number);
+        System.out.println("Czy Liczba " + number + " jest parzysta: " + even);
+
+        int number2 = 3;
+        boolean odd = math.isOdd(number2);
+        System.out.println("Czy liczba " + number2 + " jest nieparzysta: " + odd);
+
+        double radius = 2.5;
+        double circleField = math.circleField(radius);
+        System.out.println("Pole koła o promieniu " + radius + " wynosi: " + circleField);
+
+        int number3 = 4;
+        int power = math.power(number3);
+        System.out.println("Kwadrat liczby " + number3 + " wynosi: "  + power);
     }
 }
